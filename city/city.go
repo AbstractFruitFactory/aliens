b1c1f73dd039e893d62ef2656fc202847129f8c1
Alexander Wormbs <alexander.wormbs@consensys.net>
2020-01-08 20:06:45 +0000

    refactor

--- city/city.go
+++ city/city.go
@@ -1,8 +1,11 @@
 package city
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 )
 
 type CityMap struct {
@@ -121,5 +124,75 @@ func (cityMap CityMap) Invade(nbrOfAliens int) {
 	aliens := assignInvaders(nbrOfAliens, cityMap)
 	for checkMoveCount(aliens) && len(aliens) > 1 {
 		iterateInvasion(&cityMap.Cities, &aliens)
+	}
+}
+
+func BuildCityMap(filePath string) CityMap {
+	file, err := os.Open(filePath)
+	checkErr(err)
+	defer file.Close()
+
+	cityMap := CityMap{}
+	cityMap.Cities = map[string]*City{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineData := strings.Split(line, " ")
+		cityName := lineData[0]
+		var neighborNorth *City
+		var neighborEast *City
+		var neighborSouth *City
+		var neighborWest *City
+
+		for i := 1; i < len(lineData); i++ {
+			neighborData := strings.Split(lineData[i], "=")
+			direction := neighborData[0]
+			neighborCityName := neighborData[1]
+
+			var neighborCity *City
+
+			if !cityMap.HasCity(neighborCityName) {
+				cityMap.Cities[neighborCityName] = &City{Name: neighborCityName}
+				cityMap.Cities[neighborCityName].Invaders = map[int]*Alien{}
+			}
+
+			neighborCity = cityMap.Cities[neighborCityName]
+
+			switch direction {
+			case "north":
+				neighborNorth = neighborCity
+			case "east":
+				neighborEast = neighborCity
+			case "south":
+				neighborSouth = neighborCity
+			case "west":
+				neighborWest = neighborCity
+			}
+		}
+
+		if !cityMap.HasCity(cityName) {
+			cityMap.Cities[cityName] = &City{
+				Name:  cityName,
+				North: neighborNorth,
+				East:  neighborEast,
+				South: neighborSouth,
+				West:  neighborWest,
+			}
+			cityMap.Cities[cityName].Invaders = map[int]*Alien{}
+		} else {
+			cityMap.Cities[cityName].North = neighborNorth
+			cityMap.Cities[cityName].East = neighborEast
+			cityMap.Cities[cityName].South = neighborSouth
+			cityMap.Cities[cityName].West = neighborWest
+		}
+	}
+
+	return cityMap
+}
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"errors"
 	"github.com/alexmedkex/aliens/city"
 	"github.com/urfave/cli"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var app = cli.NewApp()
-
-func checkErr(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 func setInfo() {
 	app.Name = "Alien invasion simulator"
@@ -30,79 +22,17 @@ func setInfo() {
 			panic(errors.New("Invalid input. Must be a positive integer."))
 		}
 
-		cityMap := buildCityMap("./example.txt")
+		cityMap := city.BuildCityMap("./example.txt")
 		cityMap.Invade(nbrOfAliens)
 
 		return nil
 	}
 }
 
-func buildCityMap(filePath string) city.CityMap {
-	file, err := os.Open(filePath)
-	checkErr(err)
-	defer file.Close()
-
-	cityMap := city.CityMap{}
-	cityMap.Cities = map[string]*city.City{}
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		lineData := strings.Split(line, " ")
-		cityName := lineData[0]
-		var neighborNorth *city.City
-		var neighborEast *city.City
-		var neighborSouth *city.City
-		var neighborWest *city.City
-
-		for i := 1; i < len(lineData); i++ {
-			neighborData := strings.Split(lineData[i], "=")
-			direction := neighborData[0]
-			neighborCityName := neighborData[1]
-
-			var neighborCity *city.City
-
-			if !cityMap.HasCity(neighborCityName) {
-				cityMap.Cities[neighborCityName] = &city.City{Name: neighborCityName}
-				cityMap.Cities[neighborCityName].Invaders = map[int]*city.Alien{}
-			}
-
-			neighborCity = cityMap.Cities[neighborCityName]
-
-			switch direction {
-			case "north":
-				neighborNorth = neighborCity
-			case "east":
-				neighborEast = neighborCity
-			case "south":
-				neighborSouth = neighborCity
-			case "west":
-				neighborWest = neighborCity
-			}
-		}
-
-		if !cityMap.HasCity(cityName) {
-			cityMap.Cities[cityName] = &city.City{
-				Name:  cityName,
-				North: neighborNorth,
-				East:  neighborEast,
-				South: neighborSouth,
-				West:  neighborWest,
-			}
-			cityMap.Cities[cityName].Invaders = map[int]*city.Alien{}
-		} else {
-			cityMap.Cities[cityName].North = neighborNorth
-			cityMap.Cities[cityName].East = neighborEast
-			cityMap.Cities[cityName].South = neighborSouth
-			cityMap.Cities[cityName].West = neighborWest
-		}
-	}
-
-	return cityMap
-}
-
 func main() {
 	setInfo()
 	err := app.Run(os.Args)
-	checkErr(err)
+	if err != nil {
+		panic(err)
+	}
 }
