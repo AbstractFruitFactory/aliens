f6a3398352a251e218fc1c7440bb492e8464abb6
Alexander Wormbs <alexander.wormbs@consensys.net>
2020-01-08 13:34:39 +0000

    implement initiating city from file

--- main.go
+++ main.go
@@ -1,28 +1,105 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"errors"
+	"github.com/alexmedkex/aliens/graph"
 	"github.com/urfave/cli"
-	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var app = cli.NewApp()
 
-func info() {
-	app.Name = "Aragon Challenge"
-	app.Usage = "Solution to the Aragon challenge."
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func setInfo() {
+	app.Name = "Tendermint Challenge"
+	app.Usage = "Solution to the Tendermint challenge."
 	app.Version = "1.0.0"
 	app.Action = func(c *cli.Context) error {
-		fmt.Printf("Hello %q", c.Args().Get(0))
+		input := c.Args().Get(0)
+		nbrOfAliens, err := strconv.Atoi(input)
+
+		if err != nil || nbrOfAliens < 1 {
+			panic(errors.New("Invalid input. Must be an positive integer."))
+		}
+
 		return nil
 	}
 }
 
-func main() {
-	info()
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
+func buildCityMap(filePath string) graph.CityMap {
+	file, err := os.Open(filePath)
+	checkErr(err)
+	defer file.Close()
+
+	cityMap := graph.CityMap{}
+	cityMap.Cities = map[string]*graph.City{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineData := strings.Split(line, " ")
+		cityName := lineData[0]
+		var neighborNorth *graph.City
+		var neighborEast *graph.City
+		var neighborSouth *graph.City
+		var neighborWest *graph.City
+
+		for i := 1; i < len(lineData); i++ {
+			neighborData := strings.Split(lineData[i], "=")
+			direction := neighborData[0]
+			neighborCityName := neighborData[1]
+
+			var neighborCity *graph.City
+
+			if !cityMap.HasCity(neighborCityName) {
+				cityMap.Cities[neighborCityName] = &graph.City{Name: neighborCityName}
+			}
+
+			neighborCity = cityMap.Cities[neighborCityName]
+
+			switch direction {
+			case "north":
+				neighborNorth = neighborCity
+			case "east":
+				neighborEast = neighborCity
+			case "south":
+				neighborSouth = neighborCity
+			case "west":
+				neighborWest = neighborCity
+			}
+		}
+
+		if !cityMap.HasCity(cityName) {
+			cityMap.Cities[cityName] = &graph.City{
+				Name:  cityName,
+				North: neighborNorth,
+				East:  neighborEast,
+				South: neighborSouth,
+				West:  neighborWest,
+			}
+		} else {
+			cityMap.Cities[cityName].North = neighborNorth
+			cityMap.Cities[cityName].East = neighborEast
+			cityMap.Cities[cityName].South = neighborSouth
+			cityMap.Cities[cityName].West = neighborWest
+		}
 	}
+
+	return cityMap
+}
+
+func main() {
+	//cityMap := buildCityMap("./example.txt")
+	setInfo()
+
+	err := app.Run(os.Args)
+	checkErr(err)
 }
