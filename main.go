6c5bd465d8f4d13142f5bd687bd56c4c3abe0bd0
Alexander Wormbs <alexander.wormbs@consensys.net>
2020-01-09 02:17:29 +0000

    add argument for city map file

--- main.go
+++ main.go
@@ -12,17 +12,19 @@ var app = cli.NewApp()
 
 func setInfo() {
 	app.Name = "Alien invasion simulator"
-	app.Usage = "\n\nSimulate an alien invasion!\n1. Specify the city map in a separate .txt file in the current folder.\n2. Run the program with number of alien invaders as argument."
+	app.Usage = "\n\nSimulate an alien invasion!\n1. Specify the city map in a separate .txt file in the current folder.\n2. Run the program with number of alien invaders and file name as arguments."
 	app.Version = "1.0.0"
 	app.Action = func(c *cli.Context) error {
-		input := c.Args().Get(0)
-		nbrOfAliens, err := strconv.Atoi(input)
+		arg1 := c.Args().Get(0)
+		arg2 := c.Args().Get(1)
+
+		nbrOfAliens, err := strconv.Atoi(arg1)
 
 		if err != nil || nbrOfAliens < 1 {
 			panic(errors.New("Invalid input. Must be a positive integer."))
 		}
 
-		cityMap := invasion.BuildCityMap("./example.txt")
+		cityMap := invasion.BuildCityMap("./" + arg2)
 		cityMap.Invade(nbrOfAliens)
 
 		return nil
